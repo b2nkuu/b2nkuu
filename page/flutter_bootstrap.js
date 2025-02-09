a924c609297da27105f5b17602130ac70147547f
b2nkuu <ban.kuu@yahoo.com>
2025-02-09 10:37:41 +0000

    Updated information

--- page/flutter_bootstrap.js
+++ page/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1446953500"
+    serviceWorkerVersion: "2889077361"
   }
 });
