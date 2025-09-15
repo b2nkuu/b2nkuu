96d0fc44244905e4e24c49d0ca2f1da3f5ab93bc
b2nkuu <ban.kuu@yahoo.com>
2025-09-15 07:58:08 +0700

    update setting title & icon of page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,26 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
+  app: {
+    head: {
+      title: "B2NKUU ~ Profile Information",
+      meta: [{ name: "description", content: "B2NKUU ~ Profile Information" }],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
+    },
+  },
   ssr: false,
   dir: {
-    public: 'resource'
+    public: "resource",
   },
   devtools: { enabled: true },
   modules: [
-    '@nuxt/ui',
-    '@nuxt/eslint',
-    '@nuxt/scripts',
-    '@nuxt/image',
-    '@pinia/nuxt'
+    "@nuxt/ui",
+    "@nuxt/eslint",
+    "@nuxt/scripts",
+    "@nuxt/image",
+    "@pinia/nuxt",
   ],
-  css: [
-    '~/assets/css/main.css'
-  ],
+  css: ["~/assets/css/main.css"],
   pinia: {
-    storesDirs: ['./**/*.store.ts'],
+    storesDirs: ["./**/*.store.ts"],
   },
-})
+});
